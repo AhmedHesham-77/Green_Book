8ffd194d54c795c7e49280efbf5636607efb96dc
Ahmed Hesham Abd Elmohiy <96230470+AhmedHesham-77@users.noreply.github.com>
2024-05-03 20:12:09 +0300

    Create cart tab

---
+++ app/Home/Cart.js
@@ -0,0 +1,8 @@
+import { View , Text } from 'react-native';
+
+export default function Cart () {
+    return (
+        <View>
+        </View>
+    );
+};
--- app/Home/_layout.js
+++ app/Home/_layout.js
@@ -1,19 +1,25 @@
 import {Tabs} from 'expo-router';
-import {FontAwesome, Ionicons} from '@expo/vector-icons';
+import {FontAwesome, Ionicons, Feather } from '@expo/vector-icons';
 import {View} from "react-native";
 
 export default function TabsLayout() {
     return (
-        <Tabs>
+        <Tabs screenOptions={{ tabBarActiveTintColor: 'green' ,tabBarInactiveTintColor:'blue'}}>
             <Tabs.Screen name='index' options={{
                 headerTitle: 'Home',
                 title: 'Home',
-                tabBarIcon: () => <FontAwesome size={36} name="home" color='green'/>,
+                tabBarIcon: ({color}) => <FontAwesome size={36} name="home" color={color}/>,
+            }}/>
+            <Tabs.Screen name='Cart' options={{
+                headerTitle: 'Cart',
+                title: 'Cart',
+                tabBarIcon: ({color}) => <Feather name="shopping-cart" size={30} color={color} />
             }}/>
             <Tabs.Screen name='Profile' options={{
                 headerTitle: 'Profile',
                 title: 'Profile',
-                tabBarIcon: () => <Ionicons name="person-circle" size={36} color="green"/>,
+                tabBarIcon: ({color}) => <Ionicons name="person-circle" size={36} color={color}/>,
+
             }}/>
         </Tabs>
     );
