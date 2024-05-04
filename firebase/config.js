de74761cbeea34e2d3a225697d9b4dec79c8d276
Ahmed Hesham Abd Elmohiy <96230470+AhmedHesham-77@users.noreply.github.com>
2024-05-05 02:05:49 +0300

    Create storage and create upload images

--- firebase/config.js
+++ firebase/config.js
@@ -1,21 +1,51 @@
-import { initializeApp } from "firebase/app";
-import { getReactNativePersistence } from "@firebase/auth/dist/rn/index.js";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import {initializeApp} from "firebase/app";
+import {getReactNativePersistence} from "@firebase/auth/dist/rn/index.js";
+import {getAuth} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
+import {getDownloadURL, getStorage, ref, uploadBytesResumable} from "firebase/storage";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDlQxo4NMf3F40tX3-_Qq5N2gBbbgOOUMg",
-  authDomain: "greenbook-1be04.firebaseapp.com",
-  projectId: "greenbook-1be04",
-  storageBucket: "greenbook-1be04.appspot.com",
-  messagingSenderId: "958604786352",
-  appId: "1:958604786352:web:0a9e06482c620e9e365975",
+    apiKey: "AIzaSyDlQxo4NMf3F40tX3-_Qq5N2gBbbgOOUMg",
+    authDomain: "greenbook-1be04.firebaseapp.com",
+    projectId: "greenbook-1be04",
+    storageBucket: "greenbook-1be04.appspot.com",
+    messagingSenderId: "958604786352",
+    appId: "1:958604786352:web:0a9e06482c620e9e365975",
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app, {
-  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
+const storage = getStorage();
 
-export { app, db, auth };
+const uploadImage = async (folder,uri, name, onProgress) => {
+
+    const fetchResponse = await fetch(uri);
+    const theBlob = await fetchResponse.blob();
+
+    const imageRef = ref(getStorage(), `${folder}/${name}`);
+
+    const uploadTask = uploadBytesResumable(imageRef, theBlob);
+
+    return new Promise((resolve, reject) => {
+        uploadTask.on('state_changed',
+            (snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                onProgress && onProgress(progress);
+            },
+            (error) => {
+                reject(error);
+            },
+            async () => {
+                const downloadUrl = await getDownloadURL(uploadTask.snapshot.ref);
+                resolve({downloadUrl, metadata: uploadTask.snapshot.metadata});
+            }
+        );
+    })
+
+}
+
+export {app, db, auth, storage,uploadImage};
--- screens/AddProduct.js
+++ screens/AddProduct.js
@@ -1,146 +1,180 @@
-import React, {useState} from "react";
+import React, {useEffect, useState, useRef} from "react";
 import {
     View,
     TextInput,
     Text,
-    StyleSheet,
+    StyleSheet, Image,
 } from "react-native";
 import Button from "../components/Button";
 import {addProduct} from "../firebase/products";
+import {app, uploadImage} from "../firebase/config";
+import AddMedia from "../components/AddMedia";
+import * as ImagePicker from "expo-image-picker";
 
 const AddProduct = () => {
-    const [productName, setProductName] = useState("");
-    const [price, setPrice] = useState("");
-    const [description, setDescription] = useState("");
-    const [productImage, setProductImage] = useState("");
-    const [productQuantity, setProductQuantity] = useState("");
-    const [error, setError] = useState("");
+        const [productName, setProductName] = useState("");
+        const [price, setPrice] = useState("");
+        const [description, setDescription] = useState("");
+        const [productQuantity, setProductQuantity] = useState("");
+        const [error, setError] = useState("");
 
-    const clearData = () => {
-        setProductName("");
-        setPrice("");
-        setProductQuantity("");
-        setDescription("");
-    }
-    const handleAddProduct = async () => {
-        setError("");
-        if (!productName) {
-            setError("Please enter product name");
-        } else if (!/^[a-zA-Z]/.test(productName)) {
-            setError("Product name must start with a letter");
-        } else if (productName.length < 4) {
-            setError("Product name must be at least 6 characters");
-        } else if (!productQuantity) {
-            setError("Please enter quantity");
 
-        } else if (productQuantity < 0) {
-            setError("quantity should be greater than 0");
-        } else if (price < 0) {
-            setError("Price should be greater than 0");
-        }
-            // else if (!productImage) {
-            //     setError("Please add image to your product");
-            //
-        // }
-        else {
+        const imageHash = useRef('https://www12.0zz0.com/2024/05/04/22/650784712.png');
+        const imageUrl = useRef('https://www12.0zz0.com/2024/05/04/22/650784712.png');
+        const pickImage = async () => {
+            let result = await ImagePicker.launchImageLibraryAsync({
+                mediaTypes: ImagePicker.MediaTypeOptions.All,
+                allowsEditing: true,
+                aspect: [4, 3],
+                quality: 1,
+            });
+
+            if (!result.canceled) {
+                imageHash.current = result.assets[0].uri;
+            }
+        };
+
+        const uploadImageToStorage = async () => {
             try {
-                const product = {
-                    productName: productName,
-                    price: price,
-                    Quantity: productQuantity,
-                    description: description
-                };
-                const ref = await addProduct(product);
-                if (!ref) {
-                    throw Error("could not add product");
+                if (imageHash.current !== 'https://www12.0zz0.com/2024/05/04/22/650784712.png') {
+                    const uri = imageHash.current;
+                    const filename = uri.split("/").pop();
+                    const folderName = "Products";
+                    const uploadResponse = await uploadImage(folderName, uri, filename);
+                    imageUrl.current = uploadResponse.downloadUrl;
                 }
-                alert("Product added successfully.");
-                clearData();
-
-            } catch (err) {
-                setError(err.message);
-                console.log(err);
+            } catch (error) {
+                console.error("Error uploading image:", error);
             }
         }
 
 
-    };
+        const clearData = () => {
+            setProductName("");
+            setPrice("");
+            setProductQuantity("");
+            setDescription("");
+            // imageHash.current = '../assets/favicon.jpg';
+            // imageUrl.current = '../assets/favicon.jpg';
+        }
+        const handleAddProduct = async () => {
+            setError("");
+            if (!productName) {
+                setError("Please enter product name");
+            } else if (!/^[a-zA-Z]/.test(productName)) {
+                setError("Product name must start with a letter");
+            } else if (productName.length < 4) {
+                setError("Product name must be at least 6 characters");
+            } else if (!productQuantity) {
+                setError("Please enter quantity");
+            } else if (productQuantity < 0) {
+                setError("quantity should be greater than 0");
+            } else if (price < 0) {
+                setError("Price should be greater than 0");
+            } else {
+                try {
+                    await uploadImageToStorage();
+                    const product = {
+                        productName: productName,
+                        price: price,
+                        Quantity: productQuantity,
+                        description: description,
+                        ImageUrl: imageUrl.current,
+                    };
+                    const ref = await addProduct(product);
+                    if (!ref) {
+                        throw Error("could not add product");
+                    }
+                    alert("Product added successfully.");
+                    clearData();
+                } catch (err) {
+                    setError(err.message);
+                    console.log(err);
+                }
+            }
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Add product</Text>
-            <TextInput
-                placeholder="Enter product name"
-                value={productName}
-                onChangeText={setProductName}
-                style={styles.input}
-            />
-            <TextInput
-                placeholder="Enter price"
-                value={price}
-                onChangeText={setPrice}
-                style={styles.input}
-            />
-            <TextInput
-                placeholder="Enter quantity"
-                value={productQuantity}
-                onChangeText={setProductQuantity}
-                style={styles.input}
-            />
-            <TextInput
-                multiline
-                placeholder="Enter full description"
-                value={description}
-                onChangeText={setDescription}
-                style={styles.description}
-            />
-            <Button
-                title="Add"
-                textColor="white"
-                onPress={handleAddProduct}
-                styles={({pressed}) => [
-                    {opacity: pressed ? 0.2 : 1},
-                    {
-                        justifyContent: "center",
-                        alignItems: "center",
-                        borderWidth: 1,
-                        borderColor: "#a4ed80",
-                        paddingVertical: 12,
-                        paddingHorizontal: 20,
-                        marginTop: 30,
-                        marginBottom: 20,
-                        width: "80%",
-                        borderRadius: 10,
-                        backgroundColor: "#246c3a",
-                    },
-                ]}
-            />
-            <Button
-                title="Clear"
-                textColor="white"
-                onPress={clearData}
-                styles={({pressed}) => [
-                    {opacity: pressed ? 0.2 : 1},
-                    {
-                        justifyContent: "center",
-                        alignItems: "center",
-                        borderWidth: 1,
-                        borderColor: "#a4ed80",
-                        paddingVertical: 12,
-                        paddingHorizontal: 20,
-                        marginTop: 30,
-                        marginBottom: 20,
-                        width: "40%",
-                        borderRadius: 10,
-                        backgroundColor: "#bd0e13",
-                    },
-                ]}
-            />
 
-            {error ? <Text style={styles.error}>{error}</Text> : null}
-        </View>
-    );
-};
+        };
+
+        return (
+            <View style={styles.container}>
+                <Text style={styles.title}>Add product</Text>
+                <TextInput
+                    placeholder="Enter product name"
+                    value={productName}
+                    onChangeText={setProductName}
+                    style={styles.input}
+                />
+                <TextInput
+                    placeholder="Enter price"
+                    value={price}
+                    onChangeText={setPrice}
+                    style={styles.input}
+                />
+                <TextInput
+                    placeholder="Enter quantity"
+                    value={productQuantity}
+                    onChangeText={setProductQuantity}
+                    style={styles.input}
+                />
+                <TextInput
+                    multiline
+                    placeholder="Enter full description"
+                    value={description}
+                    onChangeText={setDescription}
+                    style={styles.description}
+                />
+                {/*<AddMedia/>*/}
+                <Button title="Add image" onPress={pickImage}/>
+                {/*{image && <Image source={{uri: image}} style={styles.image}/>}*/}
+                <Button
+                    title="Add"
+                    textColor="white"
+                    onPress={handleAddProduct}
+                    styles={({pressed}) => [
+                        {opacity: pressed ? 0.2 : 1},
+                        {
+                            justifyContent: "center",
+                            alignItems: "center",
+                            borderWidth: 1,
+                            borderColor: "#a4ed80",
+                            paddingVertical: 12,
+                            paddingHorizontal: 20,
+                            marginTop: 30,
+                            marginBottom: 20,
+                            width: "80%",
+                            borderRadius: 10,
+                            backgroundColor: "#246c3a",
+                        },
+                    ]}
+                />
+                <Button
+                    title="Clear"
+                    textColor="white"
+                    onPress={clearData}
+                    styles={({pressed}) => [
+                        {opacity: pressed ? 0.2 : 1},
+                        {
+                            justifyContent: "center",
+                            alignItems: "center",
+                            borderWidth: 1,
+                            borderColor: "#a4ed80",
+                            paddingVertical: 12,
+                            paddingHorizontal: 20,
+                            marginTop: 30,
+                            marginBottom: 20,
+                            width: "40%",
+                            borderRadius: 10,
+                            backgroundColor: "#bd0e13",
+                        },
+                    ]}
+                />
+
+                {error ? <Text style={styles.error}>{error}</Text> : null}
+            </View>
+        );
+    }
+;
 
 export default AddProduct;
 
@@ -199,5 +233,9 @@ const styles = StyleSheet.create({
         height: 150,
         borderRadius: 12,
         backgroundColor: "#FFF",
+    },
+    image: {
+        width: 200,
+        height: 200,
     }
 });
